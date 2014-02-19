f3294e24f9c5bf2fc7e251bc8693553ec8469fb7
raelyard <dave@raelyard.com>
2014-02-19 05:06:19 -0700

    refactored to having the implementation of the game in the game class to move toward supporting a user interface

--- TicTacToe.Tests/WhenPlayingTicTacToeGameViaGameMoves.cs
+++ TicTacToe.Tests/WhenPlayingTicTacToeGameViaGameMoves.cs
@@ -50,38 +50,69 @@ namespace TicTacToe.Tests
 
         private GameResult? Outcome(params string[] moves)
         {
-            var xSpaces = moves.Where((move, index) => index % 2 == 0);
-            var oSpaces = moves.Where((move, index) => index % 2 == 1);
-            if (SideWinsForRow(xSpaces, "A"))
+            var game = new Game();
+            foreach (var move in moves)
             {
-                return GameResult.XWins;
+                game.Move(move);
             }
-            if (SideWinsForRow(oSpaces, "A"))
-            {
-                return GameResult.OWins;
-            }
-            if (moves.Length == 9)
-            {
-                return GameResult.NoWinner;
-            }
-            return null;
-        }
-
-        private bool SideWinsForRow(IEnumerable<string> sideSpaces, string rowIdentifier)
-        {
-            return sideSpaces.Count(space => space.StartsWith(rowIdentifier)) == 3;
+            return game.OutCome;
         }
     }
 
     public class Game
     {
+        private readonly IList<string> _xSpaces;
+        private readonly IList<string> _oSpaces;
+        private bool _xMovesNext;
+
+        private int OccupiedSpacesCount
+        {
+            get { return _xSpaces.Count + _oSpaces.Count; }
+        }
+
+        public Game()
+        {
+            _xMovesNext = true;
+            _xSpaces = new List<string>();
+            _oSpaces = new List<string>();
+        }
+
         public void Move(string space)
         {
+            if (_xMovesNext)
+            {
+                _xSpaces.Add(space);
+            }
+            else
+            {
+                _oSpaces.Add(space);
+            }
+            _xMovesNext = !_xMovesNext;
         }
 
         public GameResult? OutCome
         {
-            get { return null; }
+            get
+            {
+                if (SideWinsForRow(_xSpaces, "A"))
+                {
+                    return GameResult.XWins;
+                }
+                if (SideWinsForRow(_oSpaces, "A"))
+                {
+                    return GameResult.OWins;
+                }
+                if (OccupiedSpacesCount == 9)
+                {
+                    return GameResult.NoWinner;
+                }
+                return null;
+            }
+        }
+
+        private bool SideWinsForRow(IEnumerable<string> sideSpaces, string rowIdentifier)
+        {
+            return sideSpaces.Count(space => space.StartsWith(rowIdentifier)) == 3;
         }
     }
 
