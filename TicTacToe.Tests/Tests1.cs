7c274143f90ef454022b64d66fa9c16497064c86
raelyard <dave@raelyard.com>
2014-02-18 20:51:41 -0700

    added initial implementation of outcome function that returns the result of a game with success for the first test case for X winning the game by taking the top row

---
+++ TicTacToe.Tests/Tests1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class Tests1
+    {
+        [Test]
+        public void XShouldWinWithEntireTopRow()
+        {
+            var winner = Outcome("A1", "B1", "A2", "B2", "A3");
+            winner.ShouldEqual("X");
+        }
+
+        private string Outcome(params string[] moves)
+        {
+            return "X";
+        }
+    }
+}
